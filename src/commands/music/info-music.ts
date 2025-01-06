b6e45c0ab56e0055d9450d340b433c8a7ad34ca1
Raphael Kauan <raphaelkauanoficial@gmail.com>
2025-01-05 20:58:37 -0400

    feat: adding commands for /unpause and /info

---
+++ src/commands/music/info-music.ts
@@ -0,0 +1,59 @@
+import { ApplicationCommandType, Embed, EmbedBuilder } from "discord.js";
+import { Command } from "../../settings/types/Command";
+
+export default new Command({
+  name: "info-music",
+  type: ApplicationCommandType.ChatInput,
+  description: "Informações relacionadas à reprodução de música.",
+
+  async run({ interaction }) {
+    const channelId = process.env.CHANNEL_MUSIC_ID;
+
+    if (interaction.channelId != channelId) {
+      await interaction.reply({
+        ephemeral: true,
+        content:
+          "Você está tentando executar este comando no canal errado. Por favor, utilize o canal para pedidos de música.",
+      });
+      return;
+    }
+
+    const embed = new EmbedBuilder()
+      .setColor("#FAA61A")
+      .setTitle("🎷 Comandos de Música")
+      .setDescription("Aqui estão todos os comandos disponíveis para gerenciar a música(s).")
+      .addFields(
+        {
+          name: "/play <link>",
+          value:
+            "Reproduz uma música no canal de voz a partir de um link do YouTube. Se já houver uma música tocando, o comando adicionará a nova música à fila para ser reproduzida em sequência.",
+        },
+        {
+          name: "/pause",
+          value: "Pausa a música que está sendo reproduzida no momento.",
+        },
+        {
+          name: "/unpause",
+          value: "Retoma a reprodução de uma música que está pausada.",
+        },
+        {
+          name: "/skip",
+          value: "Pula a música atual e começa a tocar a próxima na fila.",
+        },
+        {
+          name: "/fila",
+          value: "Mostra todas as músicas que estão na fila.",
+        },
+        {
+          name: "/stop",
+          value: "Para a música, limpa a fila e desconecta o bot do canal de voz.",
+        }
+      )
+      .setFooter({
+        text: "Use os comandos corretamente para aproveitar as músicas 🎶",
+        iconURL: interaction.user.displayAvatarURL(),
+      });
+
+    await interaction.reply({ ephemeral: true, embeds: [embed] });
+  },
+});
--- src/commands/music/unpause.ts
+++ src/commands/music/unpause.ts
@@ -1,10 +1,35 @@
 import { ApplicationCommandType } from "discord.js";
 import { Command } from "../../settings/types/Command";
+import { musicState } from "../../utils/functions/play-music";
 
 export default new Command({
   name: "unpause",
   type: ApplicationCommandType.ChatInput,
   description: "Retoma a música pausada no canal de voz.",
 
-  async run({ interaction }) {},
+  async run({ interaction }) {
+    const channelId = process.env.CHANNEL_MUSIC_ID;
+
+    if (interaction.channelId != channelId) {
+      await interaction.reply({
+        ephemeral: true,
+        content:
+          "Você está tentando executar este comando no canal errado. Por favor, utilize o canal para pedidos de música.",
+      });
+      return;
+    }
+
+    if (!musicState.player) {
+      await interaction.reply({ ephemeral: true, content: "Não há nenhuma música pausada no momento!" });
+      return;
+    }
+
+    if (musicState.player.state.status === "paused") {
+      musicState.player.unpause();
+
+      await interaction.reply({ content: "▶️ A música foi retomada!" });
+      return;
+    }
+    await interaction.reply({ ephemeral: true, content: "A música já está tocando!" });
+  },
 });

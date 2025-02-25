544da0d473252219a70d59c2f68c8c3c0776bed4
raphaelkauan <raphaelkauanoficial@gmail.com>
2025-02-25 10:48:20 -0400

    refactor: small change

--- src/utils/functions/validationUrl.ts
+++ src/utils/functions/validationUrl.ts
@@ -3,9 +3,8 @@ import { createEmbedInformation } from "./createEmbedInformation";
 import { colors } from "../../utils/colors/colors.json";
 
 export async function validationUrl(url: string, interaction: CommandInteraction) {
-  if (!url.includes("www.youtube.com")) {
+  if (!url.includes("https://www.youtube.com")) {
     await interaction.reply({
-      ephemeral: true,
       embeds: [
         createEmbedInformation(colors.yellow, "Informação", "Por favor, insira um link válido do YouTube"),
       ],
